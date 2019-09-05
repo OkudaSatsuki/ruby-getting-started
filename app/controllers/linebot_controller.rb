3a7eeb1f06bea2c9b1deb63d6287e757da6ec3bc
OkudaSatsuki <o.satsubon120905.os@icloud.com>
2019-09-05 15:18:57 +0000

    ピアノ画像とコードネーム送信

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -42,9 +42,9 @@ class LinebotController < ApplicationController
                         if code.name == str #入力されたコードネームと一致したら
                             message = {
                                 [type: "text",text: "#{code.first} #{code.third} #{code.fifth}"],
-                                [type: "image",originalContentUrl: "https://gakufu.gakki.me/img/codep/C/C_V1.jpg",previewImageUrl: "https://gakufu.gakki.me/img/codep/C/C_V1.jpg"]
+                                [type: "image",originalContentUrl: "https://gakufu.gakki.me/img/codep/C/C_V1.jpg",previewImageUrl: "https://gakufu.gakki.me/img/codep/C/C_V1.jpg"],
                             }
-                            break
+                        break
                         
                         else                #DB上になかったら
                             message = {
@@ -54,7 +54,7 @@ class LinebotController < ApplicationController
                         end
                     end
                 end
-                client.reply_message(event["replyToken"], message1)
+                client.reply_message(event["replyToken"], message)
                 
            when Line::Bot::Event::MessageType::Location
                message = {
